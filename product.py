2fb7fce012d216434fc249e8fec642b21a0c8fe7
EstherMwiyeria <esthermkanoro@gmail.com>
2023-05-17 12:59:15 +0300

    viewing of product

--- product.py
+++ product.py
@@ -12,3 +12,10 @@ class Cart:
 
     def add_items(self, item):
         self.products.append(item)
+    def remove_items(self, item):  # Enables the client to remove items from the cart
+        if item in self.product:
+            self.product.remove(item)
+        else:
+            print("Item not found in cart.")
+    def get_total_items(self):  # Enables the client to get the total number of items in the cart
+        return len(self.product)
