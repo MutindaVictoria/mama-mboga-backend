5fb90032d3610c9322cd41a1dcafd0e35a113253
JemimaNyaguthii <jemimanyaguthiiwambugu@gmail.com>
2023-05-17 13:22:53 +0300

    checks product

--- product.py
+++ product.py
@@ -19,3 +19,16 @@ class Cart:
             print("Item not found in cart.")
     def get_total_items(self):  # Enables the client to get the total number of items in the cart
         return len(self.product)
+
+    def total_amount_of_item(self):  # Enables the client to get the total price of items in cart
+        total_price = 0
+        for item in self.product:
+            total_price += item.price
+        return total_price
+        
+class Client:
+    def __init__(self, name, email, home_address):
+        self.name = name
+        self.email = email
+        self.home_address = home_address
+        self.cart = Cart()
