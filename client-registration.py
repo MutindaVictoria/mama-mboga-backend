9036b7f29dd8f9fad9cdfa02d5c77068760a0d44
linetmusembi <musembilinetmwongeli@gmail.com>
2023-05-20 19:12:08 +0300

    Enables client registration

--- client-registration.py
+++ client-registration.py
@@ -5,21 +5,19 @@ class Client:
         self.email = email
         self.password = password
         self.home_address = home_address
-    def sign_up():   
+    def sign_up(self):   
         name = input("Enter your name:")
         phone_number = input("Enter your phone_number:")
         email = input("Enter your email:")
         password = input("Enter your password:")
         home_address = input("Enter your home_address")
-        client = Client(name,phone_number,email,password,home_address)
+        client = Client(self,name,phone_number,email,password,home_address)
         return f"The sign-up process has been successfully completed"
-    def log_in(clients):
-        email = input("Enter your email:")
-        password = input("Enter your password:")
-        for client in clients:
-            if client.email==email and client.password==password:
+    def log_in(self,email,password):
+        #method to authenticate user's log in credentials
+        if self.email==email and self.password==password:
                 print("Login was successful")
                 
-            else:
+        else:
                 print("Login in was not unsuccessful ,try again.")
                 
